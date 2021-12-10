4544311893027e4b1ef0e32d11d84bef51697109
Robert Joseph Cox Jr <robert@studiorclv.com>
2021-12-09 21:19:32 -0800

    fixed typo

--- src/queries/patterns.ts
+++ src/queries/patterns.ts
@@ -5,7 +5,7 @@ export const insertPattern = new RegExp(
 
 /** Represents the valid parts of a select query */
 export const selectPattern = new RegExp(
-  /([selectSELECT]*) (?<requeted>[a-zA-Z\*, _]*) ([FROMfrom]*) (?<table>[a-zA-Z_]*)/g
+  /([selectSELECT]*) (?<requested>[a-zA-Z\*, _]*) ([FROMfrom]*) (?<table>[a-zA-Z_]*)/g
 );
 
 /** Represents the valid parts of an update query */
